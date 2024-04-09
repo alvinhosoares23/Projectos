c0ba3fe90470c60c8a563c2e8c42961440daa0ef
alvinhosoares23 <alvinho.soares@proton.me>
2024-04-09 16:46:22 -0100

    upadate

--- exercicios/bicicletaria.py
+++ exercicios/bicicletaria.py
@@ -5,6 +5,18 @@ class Bicicletas:
         self.ano = ano 
         self.valor = valor
 
-    
+    def buzinar(self):
+        print('Prim Prim Prim !!!!')
 
-b1 = Bicicletas('vermelha', 'caloi','2007', 30000)
+    def parar(self):
+        print("Prando bicicleta !!!")
+        print("Bicicleta Parado !!!")
+
+    def trocar_marcha(self,num_marcha):
+        print(f"Marcha Trocada para {num_marcha}")
+    
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items() ])} "
+
+b1 = Bicicletas('Azul', 'Montan Dragon','2007', 30000)
+print(b1)
