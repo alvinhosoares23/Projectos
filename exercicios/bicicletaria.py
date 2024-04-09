847570b3e805854e0f6e4756173304fbe028b66f
alvinhosoares23 <alvinho.soares@proton.me>
2024-04-09 16:28:06 -0100

    update

---
+++ exercicios/bicicletaria.py
@@ -0,0 +1,9 @@
+class Bicicletas:
+    def __init__(self, cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano 
+        self.valor = valor
+
+
+b1 = Bicicletas('vermelha', 'caloi','2007', 30000)
